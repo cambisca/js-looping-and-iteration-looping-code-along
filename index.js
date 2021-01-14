a280171eab6181fcbc978f1c958e5315dd2c3bdf
Cameron Bisca <cjbisca@gmail.com>
2021-01-14 13:47:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let nameArray = ["Ada", "Brendan", "Ali"]
+
+function writeCards(array, eventName){
+    for (let counter = 0; counter < nameArray.length; counter++){
+        console.log(`Thank you, ${nameArray[counter]}, for the wonderful ${eventName} gift!`)
+    };
+}
+
+
+function countDown(number){
+   while (number >= 0) {
+       console.log(number)
+       number --
+   }
+}
